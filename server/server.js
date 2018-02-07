c404249fa9bf990b03da77b87d461d6c2286c40c
rubertnieuwenhuis <rubertnieuwenhuis@gmail.com>
2018-02-07 05:52:23 -0500

    delete and patch routes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
 
@@ -49,7 +51,54 @@ app.get('/todos/:id', (req,res) => {
   
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('ID not valid');
+  }
+  
+  Todo.findByIdAndRemove(id).then(todo => {
+    if(!todo) {
+      return res.status(404).send('not found or removed');
+    }
+    
+    res.send(todo);
+    
+  }).catch(e => {
+    res.status(400).send(e);
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+  
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('ID not valid');
+  }
+  
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    if(!todo){
+      return res.status(404).send('not found or updated');
+    }
+    
+    res.send({todo});
+    
+  }).catch((e) => {
+   res.status(400).send(); 
+  })
+  
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 })
 
