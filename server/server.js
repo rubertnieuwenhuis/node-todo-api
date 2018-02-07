ccb7fe0594d4ffa9350dc8a7bcdbbf0c15009d7f
rubertnieuwenhuis <rubertnieuwenhuis@gmail.com>
2018-02-07 07:47:00 -0500

    after auth tokens

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ app.get('/', (req, res) => {
 app.post('/todos', (req,res) => {
   const todo = new Todo({text: req.body.text});
   todo.save().then(todo => {
-    console.log('todo: ', todo);
     res.send(todo);
   }, e => {
     res.status(400).send(e);
@@ -96,6 +95,19 @@ app.patch('/todos/:id', (req, res) => {
    res.status(400).send(); 
   })
   
+});
+
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+   
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch(e => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, () => {
